eef6a62b3231a520b67c1f9368b9b94cc24e8364
Can Tepakidareekul <panta.stu@gmail.com>
2020-09-26 22:24:24 +0800

    Add user API

---
+++ user.js
@@ -0,0 +1,14 @@
+const userDB = {};
+
+const login = (req, res) => {
+    const {user_id} = req.body
+    const data = {
+        is_new: !userDB[user_id],
+    }
+    userDB[user_id] = true
+    res.send(data)
+}
+
+module.exports = {
+    login,
+}
