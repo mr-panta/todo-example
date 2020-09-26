d75fb88f5ffd749fa99ae217197a969c335d37d7
Can Tepakidareekul <panta.stu@gmail.com>
2020-09-26 21:26:28 +0800

    'hi'

--- server.js
+++ server.js
@@ -1,18 +1,27 @@
-let http = require('http');
+const http = require('http');
+const querystring = require('querystring');
 
 const port = 8080;
 
-let getTime = (req, res) => {
+let getTime = (req, res, query) => {
     const data = {
         time: Date.now(),
     };
+    data.output = query.input * 2;
     res.write(JSON.stringify(data));
     res.end();
 };
 
 let httpHandler = (req, res) => {
-    if (req.method === "GET" && req.url === "/get-time") {
-        getTime(req, res);
+    // Prepare
+    const params = req.url.split('?');
+    let urlObj = null;
+    if (params.length > 1) {
+        urlObj = querystring.parse(params[1]);
+    }
+    // Routing
+    if (req.method === "GET" && params[0] === "/get-time") {
+        getTime(req, res, urlObj);
     } else {
         res.write("not found");
         res.end();
