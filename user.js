a9a71ed0d76c343f4070ab2b470c1cf30dc0b66a
Can Tepakidareekul <panta.stu@gmail.com>
2020-09-28 22:26:23 +0800

    Connect to database

--- user.js
+++ user.js
@@ -1,12 +1,42 @@
-const userDB = {};
+const mysql = require('mysql')
+
+const conn = mysql.createConnection({
+    host: "127.0.0.1",
+    user: "root",
+    password: "12345678",
+    database: "todo_database",
+})
 
 const login = (req, res) => {
-    const { user_id } = req.body
-    const data = {
-        is_new: !userDB[user_id],
-    }
-    userDB[user_id] = true
-    res.send(data)
+    const { username } = req.body
+    conn.query('SELECT `user_id`, `username` FROM `user_table` WHERE `username` = ?', [username], (err, results) => {
+        if (err) {
+            return res.send({
+                valid: false,
+                error: err,
+            })
+        }
+        if (results.length == 0) {
+            // Create new row
+            conn.query('INSERT INTO `user_table` (`username`) VALUES (?)', [username], (err, insertResult) => {
+                if (err) {
+                    return res.send({
+                        valid: false,
+                        error: err,
+                    })
+                }
+                return res.send({
+                    valid: true,
+                    user_id: insertResult.insertId,
+                })
+            })
+        } else {
+            return res.send({
+                valid: true,
+                user_id: results[0].user_id,
+            })
+        }
+    })
 }
 
 module.exports = {
