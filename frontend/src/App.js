58e1f51a921270d5d3085fcc46448417054bc02e
Can Tepakidareekul <panta.stu@gmail.com>
2020-10-17 21:43:09 +0800

    Update demo

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,45 +1,52 @@
 import React, { Component } from 'react';
+import { render } from 'react-dom';
 
-class App extends Component {
+class App extends Component { // stateful component
   
   constructor() {
     super()
     this.state = {
-      price: 0,
+      price: 100,
     }
-
-    this.handlePriceChange = this.handlePriceChange.bind(this);
+    // this.handlePriceChange = this.handlePriceChange.bind(this);
   }
 
-  setPrice(price) {
-    this.setState({
-      price: price,
-    })
-  }
-
-  handlePriceChange(event) {
-    let price = event.target.value
-    console.log(this, event)
-    this.setPrice(price)
-  }
+  // handlePriceChange(event) {
+  //   let price = event.target.value
+  //   this.setState({
+  //     price: price
+  //   })
+  // }
 
   render() {
     return (
-      <div>
-        <input
-          type="text"
-          onChange={this.handlePriceChange}
-          onClick={() => alert('KUY')}
+      <div>      
+        <PriceInput
+          setPrice={newPrice => this.setState({ price: newPrice })}
+          price={this.state.price}
         />
-        <App2 price={this.state.price} />
+        <PriceBox
+          currency="บาท"
+          price={this.state.price}
+        />
       </div>
     )
   }
 }
 
-const App2 = ({ price }) => {
+const PriceInput = (props) => {
   return (
-    <div>{price} baht</div>
+    <input
+      type="number"
+      onChange={event => props.setPrice(event.target.value)}
+      value={props.price}
+    />
+  )
+}
+
+const PriceBox = (props) => { // stateless component
+  return (
+    <div>{props.price} {props.currency}</div>
   )
 }
 
